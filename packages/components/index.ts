d78bcf1037e170437097a892aa142ea74370efca
1942847253 <1942847253@qq.com>
2022-08-12 22:21:47 +0800

    init: 初始化Monorepo环境 脚本自动化创建配置 编译打包配置

---
+++ packages/components/index.ts
@@ -0,0 +1,5 @@
+import YButton from './src/button'
+
+export {
+    YButton,
+}
---
+++ packages/components/src/button/button.vue
@@ -0,0 +1,47 @@
+<!-- button.vue -->
+<template>
+    <button :disabled="props.disabled"
+        :class="`${props.disabled && 'y-button-disabled'} ${!props.disabled && 'y-button'} ${!props.disabled && getButtonType(props.type)} ${getButtonSize(props.size)}`">
+        <span>
+            <slot></slot>
+        </span>
+    </button>
+</template>
+
+<script lang="ts" setup>
+import './style/index.less'
+import { defineComponent, computed } from 'vue'
+import { buttonProps } from './types'
+
+const props = defineProps(buttonProps)
+const getButtonType = (type: string | undefined) => {
+    switch (type) {
+        case 'primary':
+            return 'button-primary'
+        case 'success':
+            return 'button-success'
+        case 'danger':
+            return 'button-danger'
+        case 'warning':
+            return 'button-warning'
+        case 'info':
+            return 'button-info'
+        default:
+            return ''
+    }
+}
+
+
+const getButtonSize = (size: string | undefined) => {
+    switch (size) {
+        case 'small':
+            return 'size-small'
+        case 'default':
+            return 'size-default'
+        case 'large':
+            return 'size-large'
+        default:
+            return 'size-default'
+    }
+}
+</script>
