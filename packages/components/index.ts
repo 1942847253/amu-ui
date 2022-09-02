2e51f6c71fe8b68836115320d7dae73361193f46
1942847253 <1942847253@qq.com>
2022-09-02 22:28:53 +0800

    refactor: 移除旧版本组件仓库

--- packages/components/index.ts
+++
@@ -1,6 +0,0 @@
-import YButton from "./src/button";
-import YCheckbox from "./src/checkbox";
-import YCheckboxGroup from "./src/checkboxgroup";
-import YSwitch from "./src/switch";
-
-export { YButton, YCheckbox, YCheckboxGroup, YSwitch };
