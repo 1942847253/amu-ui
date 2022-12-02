b351c83c0580317244931d198c66bff7d7811525
‘1942847253’ <1942847253@qq.com>
2022-12-02 23:42:52 +0800

    feat: input组件新增禁用模式和一键清除

--- packages/y-ui/components/Input/index.tsx
+++ packages/y-ui/components/Input/index.tsx
@@ -1,27 +1,32 @@
 import { getStyleAttributeValue } from "../../shared/utils";
-import { computed, defineComponent, nextTick, onMounted, ref, watch } from "vue";
+import { computed, defineComponent, nextTick, onMounted, ref, unref, watch } from "vue";
 import './index.scss';
 
 export default defineComponent({
     name: 'YInput',
     props: {
         value: [String],
-        modelValue: [String],
+        modelValue: {
+            type: String,
+            default: ''
+        },
         width: [Number, String],
-        height: [Number, String]
+        height: [Number, String],
+        disabled: {
+            type: Boolean,
+            default: false
+        }
     },
     emits: ['update:modelValue'],
     setup(props, { emit }) {
-        const state = {
-            width: 0,
-            height: 0
-        }
         const inputContentRef = ref<HTMLDivElement | null>(null);
-        const inputRef = ref<HTMLDivElement | null>(null);
-        const inputContentStyle = computed(() => {
+        const inputRef = ref<HTMLInputElement | null>(null);
+        let value = unref(props.modelValue);
+        const showCloseBtn = ref(false)
+        const inputxStyle = computed(() => {
             return {
                 width: props.width ? props.width + 'px' : '100%',
-                height: props.height ? props.height + 'px' : '30px'
+                height: props.height ? props.height + 'px' : '30px',
             }
         })
 
@@ -36,19 +41,49 @@ export default defineComponent({
         const initInputWidth = () => {
             inputContentRef.value!.style.flexGrow = props.width ? '0' : '1'
         }
+
+        const clearValue = () => {
+            value = ''
+            emit('update:modelValue', value);
+            setTimeout(() => {
+                inputRef.value!.focus();   
+            });
+        }
+
+        const changeInputValue = (e: Event) => {
+            const target = e.target as HTMLInputElement
+            showCloseBtn.value = true     
+            value = target.value
+            emit('update:modelValue',value);
+        }
+        const closedBtn = () => {
+            return (
+                <div class="y-input-close">
+                    <span onMousedown={() => clearValue()} class="iconfont icon-guanbi"></span>
+                </div>
+            )
+        }
         return () => (
-            <div class="y-input-content">
-                <div class="y-input-wrapper" ref={inputContentRef}>
+            <div class="y-input-content" style={`cursor: ${props.disabled ? 'no-drop' : ''};`}>
+                <div class="y-input-wrapper" style={{
+                    backgroundColor: (props.disabled ? "#f5f7fa" : ''),
+                    pointerEvents: (props.disabled ? 'none' : 'auto'),
+                }} ref={inputContentRef}>
                     <input
-                        style={inputContentStyle.value}
+                        style={inputxStyle.value}
                         placeholder="Please input"
                         class="input"
+                        onInput={changeInputValue}
+                        onFocus={() => props.modelValue.length > 0 && (showCloseBtn.value = true)}
+                        onBlur={() => { showCloseBtn.value = false}}
+                        disabled={props.disabled}
                         type="text"
-                        value={props.value}
+                        value={value}
                         ref={inputRef}
                     />
-                </div>
 
+                </div>
+                {showCloseBtn.value && closedBtn()}
             </div>
         )
     }
