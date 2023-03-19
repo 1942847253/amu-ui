771a91788f94e486515df0c738daf6eb4cfe24c9
1942847253 <1942847253@qq.com>
2023-03-19 18:05:51 +0800

    build: publish  path change

--- packages/components/script/publish/index.ts
+++ packages/components/script/publish/index.ts
@@ -2,6 +2,6 @@ import run from "../utils/run";
 import { pkgPath } from "../utils/paths";
 import { series } from "gulp";
 export const publishComponent = async () => {
-  run("release-it", `${pkgPath}/yui-test`);
+  run("release-it", `${pkgPath}/amu-ui`);
 };
 export default series(async () => publishComponent());
