558ff91fe2c29c41a43811c0dfea4758ee460c9a
‘1942847253’ <1942847253@qq.com>
2023-03-27 21:00:10 +0800

    docs: 文档新增input-number组件

---
+++ site/docs/examples/input-number/component/Basic.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-input-number v-model="input" placeholder="Please input" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const input = ref(520)
+</script>
+
+<style lang="less" scoped>
+:deep(.subtract-button){
+  height: 29.5px;
+}
+:deep(.add-button){
+   height: 29.5px;
+}
+</style>
