789ea61fddfe607752c19ca55bf37a8835a75d1c
1942847253 <1942847253@qq.com>
2026-03-08 20:13:05 +0800

    fix: stabilize component declaration output

--- packages/components/breadcrumb/index.ts
+++ packages/components/breadcrumb/index.ts
@@ -1,12 +1,17 @@
 import { withInstall, withNoopInstall } from '@amu-ui/utils'
+import type { SFCWithInstall } from '@amu-ui/utils'
 import Breadcrumb from './src/breadcrumb.vue'
 import BreadcrumbItem from './src/breadcrumb-item.vue'
 import './src/style.css'
 
-export const AmuBreadcrumb = withInstall(Breadcrumb, {
+type BreadcrumbWithInstall = SFCWithInstall<typeof Breadcrumb> & {
+  BreadcrumbItem: typeof BreadcrumbItem
+}
+
+export const AmuBreadcrumb: BreadcrumbWithInstall = withInstall(Breadcrumb, {
   BreadcrumbItem,
 })
-export const AmuBreadcrumbItem = withNoopInstall(BreadcrumbItem)
+export const AmuBreadcrumbItem: SFCWithInstall<typeof BreadcrumbItem> = withNoopInstall(BreadcrumbItem)
 export default AmuBreadcrumb
 
 export * from './src/props'
--- packages/components/collapse/index.ts
+++ packages/components/collapse/index.ts
@@ -1,12 +1,17 @@
 import { withInstall, withNoopInstall } from '@amu-ui/utils'
+import type { SFCWithInstall } from '@amu-ui/utils'
 import Collapse from './src/collapse.vue'
 import CollapseItem from './src/collapse-item.vue'
 import './src/style.css'
 
-export const AmuCollapse = withInstall(Collapse, {
+type CollapseWithInstall = SFCWithInstall<typeof Collapse> & {
+  CollapseItem: typeof CollapseItem
+}
+
+export const AmuCollapse: CollapseWithInstall = withInstall(Collapse, {
   CollapseItem,
 })
-export const AmuCollapseItem = withNoopInstall(CollapseItem)
+export const AmuCollapseItem: SFCWithInstall<typeof CollapseItem> = withNoopInstall(CollapseItem)
 export default AmuCollapse
 
 export * from './src/props'
